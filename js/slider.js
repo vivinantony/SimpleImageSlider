3c7ec3c5f8afd01004911bfe8ca7de599ad03add
Vivin <vivin@poolcircle.com>
2015-02-21 18:30:28 +0530

    New

---
+++ js/slider.js
@@ -0,0 +1,16 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: VIVIN
+ * Date: 5/25/14
+ * Time: 1:32 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+$('#slider').cycle({
+    fz:         'scrollHorz',
+    next:       '#next',
+    prev:       '#prev',
+    pager: 		'#pager', 
+    timeout:    3000,
+    pause:      1
+});
